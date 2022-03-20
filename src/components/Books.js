2809be895cbe4b203fa4a73f53ee5b198e275cf5
rishabh dhiman <dhiman.rishabh.1998@gmail.com>
2022-03-20 16:26:45 +0530

    add all changes

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
 import backImg from "../assets/Back.svg";
-import searchImg from "../assets/Search.svg";
 import "../styles/books.css";
 import book1 from "../assets/book1.jpg";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { MdClear } from "react-icons/md";
+import { MdSearch } from "react-icons/md";
 
 const Books = () => {
+  const navigate = useNavigate();
+  const routeChange = () => {
+    navigate(`/`);
+  };
+
+  const [searchTerm, setSearchTerm] = useState("");
+
   const { title } = useParams();
   const [booksCollection, setbooksCollections] = useState([]);
 
@@ -24,34 +33,77 @@ const Books = () => {
     getBooks();
   }, []);
 
+  const handle = (event) => {
+    document.getElementById("header-search").value = "";
+  };
+
   return (
     <div class="conatiner-fluid">
       <div class="row books-row">
-        <h1>{title}</h1>
+        <div className="category-title">
+          <img
+            src={backImg}
+            alt="backImg"
+            height="25px"
+            width="25px"
+            onClick={routeChange}
+          />
+          <h1>{title}</h1>
+        </div>
+
         <div class="searchBar">
-          {/* <img src={searchImg} alt="search"/>  */}
           <input
             type="text"
             id="header-search"
             placeholder="Search"
             name="s"
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
           ></input>
+
+          <div class="clear-icon">
+            {searchTerm.length !== 0 ? (
+              <MdClear id="clear-btn" onClick={handle} />
+            ) : (
+              <MdSearch />
+            )}
+          </div>
         </div>
-        {booksCollection.map((curElem) => {
-          return (
-            <div class="col-lg-2 col-md-4 col-sm-4 col-xs-4">
-              <div class="bookcard">
-                <img src={book1} />
-                <h6>{curElem["title"]}</h6>
-                <p>
-                  {curElem["authors"].map((elm) => {
-                    return elm["name"];
-                  })}
-                </p>
+        {booksCollection
+          .filter((val) => {
+            if (searchTerm === "") {
+              return val;
+            } else if (
+              val.authors
+                .map((i) => {
+                  return i["name"].toLowerCase();
+                })
+                .includes(searchTerm.toLowerCase())
+            ) {
+              return val;
+            } else if (
+              val.title.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((curElem) => {
+            return (
+              <div class="col-lg-2 col-md-4 col-sm-4 col-xs-4">
+                <div class="bookcard">
+                  <img src={book1} />
+
+                  <h6>{curElem["title"]}</h6>
+                  <p>
+                    {curElem["authors"].map((elm) => {
+                      return elm["name"];
+                    })}
+                  </p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
